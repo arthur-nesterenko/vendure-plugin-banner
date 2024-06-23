f4194765908863fb2454aac6c9967599611f29cd
Arthur <arthur.nesterenko.dev@gmail.com>
2024-06-23 10:46:23 +0300

    feat: add dev server

--- dev-server/vendure-config.ts
+++ dev-server/vendure-config.ts
@@ -1,75 +1,76 @@
 import {
-  dummyPaymentHandler,
-  VendureConfig,
-  DefaultJobQueuePlugin,
-} from "@vendure/core";
-import { AssetServerPlugin } from "@vendure/asset-server-plugin";
-import { AdminUiPlugin } from "@vendure/admin-ui-plugin";
-import path from "path";
-import { BannerPlugin } from "../src";
-// import { compileUiExtensions } from "@vendure/ui-devkit/compiler";
+    dummyPaymentHandler,
+    VendureConfig,
+    DefaultJobQueuePlugin,
+    DefaultSearchPlugin,
+} from '@vendure/core';
+import { AssetServerPlugin } from '@vendure/asset-server-plugin';
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import path from 'path';
+import { BannerPlugin } from '../src';
+import { compileUiExtensions } from '@vendure/ui-devkit/compiler';
 
 export const headlessConfig: VendureConfig = {
-  apiOptions: {
-    port: 4000,
-    adminApiPath: "admin-api",
-    adminApiPlayground: {
-      settings: {
-        "request.credentials": "include",
-      } as any,
+    apiOptions: {
+        port: 4000,
+        adminApiPath: 'admin-api',
+        adminApiPlayground: {
+            settings: {
+                'request.credentials': 'include',
+            } as any,
+        },
+        adminApiDebug: true,
+        shopApiPath: 'shop-api',
+        shopApiPlayground: {
+            settings: {
+                'request.credentials': 'include',
+            } as any,
+        },
+        shopApiDebug: true,
     },
-    adminApiDebug: true,
-    shopApiPath: "shop-api",
-    shopApiPlayground: {
-      settings: {
-        "request.credentials": "include",
-      } as any,
+    authOptions: {
+        superadminCredentials: {
+            identifier: 'superadmin',
+            password: 'superadmin',
+        },
+        cookieOptions: {
+            secret: 'secret',
+        },
     },
-    shopApiDebug: true,
-  },
-  authOptions: {
-    superadminCredentials: {
-      identifier: "superadmin",
-      password: "superadmin",
+    dbConnectionOptions: {
+        type: 'better-sqlite3',
+        synchronize: true,
+        logging: false,
+        migrations: [path.join(__dirname, '../migrations/*.ts')],
+        database: path.join(__dirname, '../banner-vendure.sqlite'),
     },
-    cookieOptions: {
-      secret: "secret",
+    paymentOptions: {
+        paymentMethodHandlers: [dummyPaymentHandler],
     },
-  },
-  dbConnectionOptions: {
-    type: "better-sqlite3",
-    synchronize: true, // turn this off for production
-    logging: false,
-    // database: "vendure-plugin-banner",
-    migrations: [path.join(__dirname, "../migrations/*.ts")],
-    database: path.join(__dirname, "../banner-vendure.sqlite"),
-  },
-  paymentOptions: {
-    paymentMethodHandlers: [dummyPaymentHandler],
-  },
-  customFields: {},
-  plugins: [
-    AssetServerPlugin.init({
-      route: "assets",
-      assetUploadDir: path.join(__dirname, "./static/assets"),
-    }),
-    BannerPlugin,
-    DefaultJobQueuePlugin,
-  ],
+    customFields: {},
+    plugins: [
+        AssetServerPlugin.init({
+            route: 'assets',
+            assetUploadDir: path.join(__dirname, './static/assets'),
+        }),
+        DefaultJobQueuePlugin,
+        DefaultSearchPlugin,
+        BannerPlugin,
+    ],
 };
 
 export const config: VendureConfig = {
-  ...headlessConfig,
-  plugins: [
-    ...(headlessConfig.plugins || []),
-    AdminUiPlugin.init({
-      route: "admin",
-      port: 4002,
-      // app: compileUiExtensions({
-      //   outputPath: path.join(__dirname, "admin-ui"),
-      //   devMode: true,
-      //   // extensions: [PagesPlugin.ui],
-      // }),
-    }),
-  ],
+    ...headlessConfig,
+    plugins: [
+        ...(headlessConfig.plugins || []),
+        AdminUiPlugin.init({
+            route: 'admin',
+            port: 4002,
+            app: compileUiExtensions({
+                outputPath: path.join(__dirname, 'admin-ui'),
+                devMode: true,
+                extensions: [BannerPlugin.ui],
+            }),
+        }),
+    ],
 };
